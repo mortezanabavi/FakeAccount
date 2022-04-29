e8b7c730d1fd876756bad06094eba4ab95261635
SingleDev <mortezanabavi93@gmail.com>
2022-04-29 22:19:29 +0430

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function ClearCookie(){
     document.cookie = '';
 }
 function SignUp(){
-    window.open("https://waitlist.stackedinvest.com/invest?kid=2BGR4H", "_self");
     window.onload = function(){
         var email = 'mortezanabavi'+Math.floor(Math.random() * 900)+'@gmail.com';
         document.getElementById("email").value = email;
